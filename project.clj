93de14b22baa8e93c4f4bbe4fb9f653ef58232b3
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-10-24 04:56:45 +0900

    project.clj add description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.hozumi/clj-reduce+ "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Clj-reduce+ can take more than two sequence as arg."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
